5a3deb152fef6ac45a659875c0b3f645a584c129
Uppalapati Syam Karni Datta <uppalapati.datta@bounteous.com>
2025-05-26 12:21:39 +0530

    added prime number func

--- GitEven.java
+++ GitEven.java
@@ -1,9 +1,16 @@
 public class GitEven {
     public static void main(String[] args) {
         int[] arr={1,4,3,6,8,9,10};
+        int count=0;
         for(int i=0;i<arr.length;i++){
             if(arr[i]%2==0) System.out.println("Even - "+arr[i]);
             else System.out.println("Odd - "+arr[i]);
+            for(int j=1;j<i;j++){
+                if(i%j==0){
+                    count++;
+                }
+            }
         }
+        System.out.println("Total number of prime numbers in the array: "+count);
     }
 }
